4146ec992ce4f5ad17f0a39f6374ac775f367841
Reham236 <reham.salah2362@gmail.com>
2025-11-30 16:25:45 +0200

    Contest1#Problem H

--- contest1.cpp
+++ contest1.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include <bitset>
 #include <string>
+#include <cmath>
+#include <climits>
 using namespace std;
 int main() {
 //  // Problem D Ali Baba Puzzle
@@ -34,42 +36,60 @@ int main() {
 // unsigned int result = num1^num2;
 //  cout<<result<<endl;
 
-// Problem G. Katryoshka  (https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/G)
-long long  n; //Eyes
-long long  m; //Mouths
-long long k; //Bodies
-long long result =0 ;
-cin>>n>>m>>k;
-if(n==0||k==0){
-    cout<<0 ;
-}else if(m==0){
-    if(n/2 <k){
-        cout<<n/2 ;
-    }else{
-        cout<<k ;
-    }
-}else if(m <= n && m <= k){
+// // Problem G. Katryoshka  (https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/G)
+// long long  n; //Eyes
+// long long  m; //Mouths
+// long long k; //Bodies
+// long long result =0 ;
+// cin>>n>>m>>k;
+// if(n==0||k==0){
+//     cout<<0 ;
+// }else if(m==0){
+//     if(n/2 <k){
+//         cout<<n/2 ;
+//     }else{
+//         cout<<k ;
+//     }
+// }else if(m <= n && m <= k){
    
-       result += m; 
-       n -= m;
-       k -= m;
-    if(n/2 <k){
-       cout<<result + n/2 ;
-    }else{
-        cout<<result + k ;
-    }
-}else if(n <= m && n <= k){
+//        result += m; 
+//        n -= m;
+//        k -= m;
+//     if(n/2 <k){
+//        cout<<result + n/2 ;
+//     }else{
+//         cout<<result + k ;
+//     }
+// }else if(n <= m && n <= k){
    
-       result += n; 
-       m -= n;
-       k -= n;
-       cout<<result ;
-}else if( k <= m && k <= n){
+//        result += n; 
+//        m -= n;
+//        k -= n;
+//        cout<<result ;
+// }else if( k <= m && k <= n){
        
-       result += k; 
-       n -= k;
-       m -= k;
-       cout<<result ;
+//        result += k; 
+//        n -= k;
+//        m -= k;
+//        cout<<result ;
+// }
+
+// Problem H. Data Type Guessing     (https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/H)
+
+long long n, k, a;
+cin >> n >> k >> a;
+long double result = (long double)n * (long double)k / (long double)a;
+cout << result << endl;
+// If there's a fractional part -> double
+if (fmod((double)result, 1.0) != 0.0) {
+  cout << "double";
+} else {
+  // No fractional part: check integer range
+  if (result > INT_MAX || result < INT_MIN) {
+    cout << "long long";
+  } else {
+    cout << "int";
+  }
 }
     return 0;
 }
