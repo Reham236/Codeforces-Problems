c00992c855cc87a5f10641a9efb751208b23263c
Reham236 <reham.salah2362@gmail.com>
2026-02-21 04:51:47 +0200

    Sheet#3 Problem L

--- sheet3.cpp
+++ sheet3.cpp
@@ -1,37 +1,106 @@
 #include<iostream>
 #include <vector>
+#include<cmath>
+#include<algorithm>
+#include <string>
+#include <cctype>
 using namespace std;
 int main(){
-   int T ;
-  
-  cin>>T;
+
+  // // Problem I. Smallest Pair    (https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/I)
+  //  int T ;
+  // cin>>T;
  
 
-  while(T>0){
-     int N ;
-     cin>>N;
-       vector<int> arr( N );
-    for (int i = 0; i < N; i++)
-    {
-        cin>>arr[i];
-    }
+  // while(T>0){
+  //    int N ;
+  //    cin>>N;
+  //      vector<int> arr( N );
+  //   for (int i = 0; i < N; i++)
+  //   {
+  //       cin>>arr[i];
+  //   }
     
-    int i=0;
-    int j=1;
-    int smallestVal=arr[i]+arr[j]+(j+1)-(i+1);
-    for (; i < j; i++)
-    { j=i+1;
-       for (; j < N; j++)
-       { int curVal=arr[i]+arr[j]+(j+1)-(i+1);
+  //   int i=0;
+  //   int j=1;
+  //   int smallestVal=arr[i]+arr[j]+(j+1)-(i+1);
+  //   for (; i < j; i++)
+  //   { j=i+1;
+  //      for (; j < N; j++)
+  //      { int curVal=arr[i]+arr[j]+(j+1)-(i+1);
        
-        if(curVal < smallestVal ){
-          smallestVal=curVal;
-        }
-       }
+  //       if(curVal < smallestVal ){
+  //         smallestVal=curVal;
+  //       }
+  //      }
        
-    }
+  //   }
    
-  cout <<smallestVal<<endl;
-    T--;
-  } 
+  // cout <<smallestVal<<endl;
+  //   T--;
+  // } 
+
+
+
+
+  // Problem L. Max Subarray (https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/L)
+//   int T ;
+//   cin>>T;
+//   while (T>0)
+//   { int N;
+//     cin>>N;
+//     vector<int> arr(N);
+//     for (int i = 0; i < N; i++)
+//     {
+//         cin>>arr[i];
+//     }
+//     for (int i = 0; i < N; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+    
+//     for (int i = 0; i < N; i++)
+//     {
+//       int j=i+1;
+//       cout<<max(arr[i],arr[j])<<" ";
+       
+//     }
+//  for (int i = 0; i < N-2; i++)
+//     {
+     
+//       int maximum=max(arr[i],arr[i+1]);
+//       cout<<max(maximum,arr[i+2])<<" ";
+       
+//     }
+// cout<<endl;
+//     T--;
+//   }
+  
+
+//Problem 
+int A ,B;
+cin>>A>>B;
+string s;
+cin >>s;
+if(s.size()!= A+B+1 || s[A]!='-'){
+    cout<<"No";
+    return 0;
+}
+ 
+for(int i=0;i<A;i++){
+   
+     if(!isdigit(s[i])){ 
+        cout<<"No";
+       return 0;
+      }
+ } 
+ for(int i=A+1;i<A+B+1;i++){
+   
+     if(!isdigit(s[i])){ 
+        cout<<"No";
+       return 0;
+      }
+ }
+ cout<<"Yes";  
+ return 0;
 }
